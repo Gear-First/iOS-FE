b572d2a9529b3a7ad3d2d267eb6d7a5e8fcf86a4
P-WJ <pwj1103@gmail.com>
2025-09-29 16:23:31 +0900

    first commit

---
+++ iOS-FE/iOS_FEApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_FEApp.swift
+//  iOS-FE
+//
+//  Created by wj on 9/29/25.
+//
+
+import SwiftUI
+
+@main
+struct iOS_FEApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
