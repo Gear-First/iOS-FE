e2e5e01698c519d5efb9f97f751e22a0dd8de36f
P-WJ <pwj1103@gmail.com>
2025-10-13 18:57:45 +0900

    feat: 차량, 부품, 수량, 날짜, 포맷터, 주문 생성, 유효성 검사, 폼 초기화 구현

---
+++ iOS-FE/Features/View/CarSearchSheetView.swift
@@ -0,0 +1,18 @@
+//
+//  CarSearchSheetView.swift
+//  iOS-FE
+//
+//  Created by wj on 10/13/25.
+//
+
+import SwiftUI
+
+struct CarSearchSheetView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CarSearchSheetView()
+}
---
+++ iOS-FE/Features/View/PartSearchSheetView.swift
@@ -0,0 +1,18 @@
+//
+//  PartSearchSheetView.swift
+//  iOS-FE
+//
+//  Created by wj on 10/13/25.
+//
+
+import SwiftUI
+
+struct PartSearchSheetView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PartSearchSheetView()
+}
